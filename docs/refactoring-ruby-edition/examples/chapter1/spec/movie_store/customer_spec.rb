4240419c0c1d4cfca47ec8cf73caeed2415d9bc2
Luke Moorman <luke.moorman@daveramsey.com>
2022-10-04 17:17:06 -0500

    Initialize tests

--- docs/refactoring-ruby-edition/examples/chapter1/spec/movie_store/customer_spec.rb
+++ docs/refactoring-ruby-edition/examples/chapter1/spec/movie_store/customer_spec.rb
@@ -1,10 +1,12 @@
 require "movie_store/customer"
 
 describe MovieStore::Customer do
-  it "initializes with the expected values" do
-    customer = described_class.new("Luke Moorman")
+  describe "#initialize" do
+    it "initializes with the expected values" do
+      customer = described_class.new("Luke Moorman")
 
-    expect(customer).to be_a(MovieStore::Customer)
-    expect(customer.name).to eq("Luke Moorman")
+      expect(customer).to be_a(MovieStore::Customer)
+      expect(customer.name).to eq("Luke Moorman")
+    end
   end
 end
--- docs/refactoring-ruby-edition/examples/chapter1/spec/movie_store/rental_spec.rb
+++ docs/refactoring-ruby-edition/examples/chapter1/spec/movie_store/rental_spec.rb
@@ -1,16 +1,18 @@
 require "movie_store/rental"
 
 describe MovieStore::Rental do
-  it "initializes with the expected values" do
-    movie = MovieStore::Movie.new("A New Hope", MovieStore::Movie::REGULAR)
+  describe "#initialize" do
+    it "initializes with the expected values" do
+      movie = MovieStore::Movie.new("A New Hope", MovieStore::Movie::REGULAR)
 
-    rental = described_class.new(movie, 270)
+      rental = described_class.new(movie, 270)
 
-    expect(rental).to be_a(MovieStore::Rental)
+      expect(rental).to be_a(MovieStore::Rental)
 
-    expect(rental.movie.title).to eq("A New Hope")
-    expect(rental.movie.price_code).to eq(0)
+      expect(rental.movie.title).to eq("A New Hope")
+      expect(rental.movie.price_code).to eq(0)
 
-    expect(rental.days_rented).to eq(270)
+      expect(rental.days_rented).to eq(270)
+    end
   end
 end
