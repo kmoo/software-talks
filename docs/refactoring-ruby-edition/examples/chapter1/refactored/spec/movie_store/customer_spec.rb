351133fb6d89c8c33807e766e01bdcb747e1c144
Luke Moorman <luke.moorman@daveramsey.com>
2022-10-05 11:07:00 -0500

    HTML Statement and tests

--- docs/refactoring-ruby-edition/examples/chapter1/refactored/spec/movie_store/customer_spec.rb
+++ docs/refactoring-ruby-edition/examples/chapter1/refactored/spec/movie_store/customer_spec.rb
@@ -27,4 +27,23 @@ describe MovieStore::Customer do
       end
     end
   end
+
+  describe "#html_statement" do
+    context "with no rentals" do
+      it "displays a blank statement" do
+        expect(subject.html_statement).to eq("<h1>Rentals for <em>Luke Moorman</em><h1><p>\n<p>You owe <em>0</em><p>\nOn this rental you earned <em>0</em> frequent renter points<p>")
+      end
+    end
+
+    context "with rentals" do
+      let(:movie) { MovieStore::Movie.new("The Gooneys", MovieStore::Movie::CHILDRENS) }
+      let(:rental) { MovieStore::Rental.new(movie, 5) }
+
+      it "displays a statement with totals" do
+        subject.add_rental(rental)
+
+        expect(subject.html_statement).to eq("<h1>Rentals for <em>Luke Moorman</em><h1><p>\n\tThe Gooneys: 4.5<br>\n<p>You owe <em>4.5</em><p>\nOn this rental you earned <em>1</em> frequent renter points<p>")
+      end
+    end
+  end
 end
