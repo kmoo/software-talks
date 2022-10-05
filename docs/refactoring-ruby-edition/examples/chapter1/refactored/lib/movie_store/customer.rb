c622a7f233809924d89e9ddc084f4a7d7ffa109c
Luke Moorman <luke.moorman@daveramsey.com>
2022-10-05 09:41:29 -0500

    Replace temp with query

--- docs/refactoring-ruby-edition/examples/chapter1/refactored/lib/movie_store/customer.rb
+++ docs/refactoring-ruby-edition/examples/chapter1/refactored/lib/movie_store/customer.rb
@@ -16,8 +16,6 @@ module MovieStore
       total_amount, frequent_renter_points = 0, 0
       result = "Rental Record for #{@name}\n"
       @rentals.each do |element|
-        this_amount = element.charge
-
         # add frequent renter points
         frequent_renter_points += 1
         # add a bonus for a two day new release rental
@@ -26,8 +24,8 @@ module MovieStore
           frequent_renter_points += 1
         end
         # show figures for this rental
-        result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-        total_amount += this_amount
+        result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+        total_amount += element.charge
       end
       # add footer lines
       result += "Your amount owed is #{total_amount}\n"
