f67e627555bdffcc50c7b2456bc2d211a673e477
Luke Moorman <luke.moorman@daveramsey.com>
2022-10-05 08:16:55 -0500

    Extract method

--- docs/refactoring-ruby-edition/examples/chapter1/refactored/lib/movie_store/rental.rb
+++ docs/refactoring-ruby-edition/examples/chapter1/refactored/lib/movie_store/rental.rb
@@ -6,5 +6,22 @@ module MovieStore
     def initialize(movie, days_rented)
       @movie, @days_rented = movie, days_rented
     end
+
+    def charge
+      result = 0
+
+      case movie.price_code
+      when Movie::REGULAR
+        result += 2
+        result += (days_rented - 2) * 1.5 if days_rented > 2
+      when Movie::NEW_RELEASE
+        result += days_rented * 3
+      when Movie::CHILDRENS
+        result += 1.5
+        result += (days_rented - 3) * 1.5 if days_rented > 3
+      end
+
+      result
+    end
   end
 end
